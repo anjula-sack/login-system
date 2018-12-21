2e40f5439c3bc6f98d4957f8d4bd79535b07dc18
anjula <anjulashanaka@gmail.com>
2018-12-21 23:19:34 +0530

    thats all for today

--- home.php
+++ home.php
@@ -1,4 +1,17 @@
 <?php
+if (!isset($_POST['username']) || !isset($_POST['password'])) {
+    header('Location: login.php?logged=false');
+    exit();
 
-$username = $_GET['username'];
-echo "welcome ",$username;
+}
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if (!($username == "admin" && $password == "pass")) {
+    header('Location: login.php?logged=false');
+    exit();
+}
+
+echo "welcome ", $username . "<br/>";
+echo "password ", $password . "<br/>";
+echo '<a href="login.php">sign out</a>';
